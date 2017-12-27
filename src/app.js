1ac10a27dc5812655b4e6094b9a87bf352a76d2c
James Kaupert <jameskaupert@gmail.com>
2017-12-27 04:42:22 -0500

    add initial boilerplate

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import DrumMachineApp from "./components/DrumMachineApp";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
+
+ReactDOM.render(
+  <div>
+    <DrumMachineApp />
+  </div>,
+  document.getElementById('app')
+);
---
+++ src/components/DrumMachineApp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class DrumMachineApp extends React.Component {
+    render() {
+        return (
+            <div className="drum-machine-app">
+            <h1>Hello Drum Machine</h1>
+            </div>
+        )
+    }
+}
